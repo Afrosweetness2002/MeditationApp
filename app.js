4218fab542ae83b4f4ce372ea207de8c70460211
Abdullatif <abdullatiffarah21@gmail.com>
2022-05-20 00:37:26 +0100

    So far got the clock, the timer, the sound, the video, and the different time buttons to work.

---
+++ app.js
@@ -0,0 +1,65 @@
+const app = function () {
+  let song = document.querySelector(".song");
+  let play = document.querySelector(".play");
+  let outline = document.querySelector(".moving-outline circle");
+  let video = document.querySelector(".vid-container video");
+
+  // sounds
+  let sounds = document.querySelectorAll(".sound-picker button");
+  //Time Display
+  let timeSelect = document.querySelectorAll(".time-select button")
+  let timeDisplay = document.querySelector(".time-display");
+  //Get the length of the outline:
+  let outlineLength = outline.getTotalLength();
+  //Duration
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //Play Sound
+  play.addEventListener("click", function () {
+    checkPlaying(song);
+  });
+
+
+//Select Sound 
+timeSelect.forEach(option => {
+    option.addEventListener("click", function(){
+        fakeDuration = this.getAttribute("data-time");
+        timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60 )}`;
+    })
+
+})
+
+  // Create a function specific to stop and play the sounds
+  let checkPlaying = (song) => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
+    }
+  };
+
+
+  // We can animate the circle
+  song.ontimeupdate = function(){
+      let currentTime = song.currentTime;
+      let elapsed = fakeDuration - currentTime;
+      let seconds = Math.floor(elapsed % 60);
+      let minutes = Math.floor(elapsed / 60);
+
+      // animate the Circle
+      let progress = outlineLength - (currentTime / fakeDuration) * outlineLength; 
+      outline.style.strokeDashoffset = progress; 
+
+      //Animate the textAlign: 
+      timeDisplay.textContent = `${minutes}:${seconds}`;
+  }
+};
+
+app();
